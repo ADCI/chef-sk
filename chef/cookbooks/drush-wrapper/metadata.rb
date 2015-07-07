4ca87f5373ce4d0e94d35d0ce9c8bffe67b5a185
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-07 21:59:18 +0600

    metadata cleanup

--- chef/cookbooks/drush-wrapper/metadata.rb
+++ chef/cookbooks/drush-wrapper/metadata.rb
@@ -3,7 +3,6 @@ maintainer       "Sergey Sergin (ADCI)"
 maintainer_email "sergey-sergin@kruhak.name"
 license          "All rights reserved"
 description      "Install Drush with dependencies"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "drush"
--- chef/cookbooks/nginx-wrapper/metadata.rb
+++ chef/cookbooks/nginx-wrapper/metadata.rb
@@ -3,7 +3,6 @@ maintainer       "Sergey Sergin (ADCI)"
 maintainer_email "sergey-sergin@kruhak.name"
 license          "All rights reserved"
 description      "Configure NGINX virtual hosts"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 depends "nginx"
