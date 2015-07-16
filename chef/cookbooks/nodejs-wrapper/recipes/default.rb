0905132ef11f57cddbc54c3f7363419e93b00650
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-16 16:21:17 +0600

    delete local installs for npm packages

--- chef/cookbooks/nodejs-wrapper/recipes/default.rb
+++ chef/cookbooks/nodejs-wrapper/recipes/default.rb
@@ -1,10 +1,5 @@
 include_recipe 'nodejs'
 
-bash 'install_webdriverio'  do
-  code "su vagrant -l -c 'npm install webdriverio'"
-end
-
-bash 'install_mocha'  do
-  code "su vagrant -l -c 'npm install mocha'"
-end
+nodejs_npm "webdriverio"
+nodejs_npm "mocha"
 
