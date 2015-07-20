873b822c1f39ead05d50bf6f07c9eb4ce5603559
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-20 20:57:10 +0600

    directory permissions and refactoring

--- chef/cookbooks/nginx-wrapper/recipes/default.rb
+++ chef/cookbooks/nginx-wrapper/recipes/default.rb
@@ -9,29 +9,50 @@
 
 include_recipe "nginx"
 
-require 'fileutils'
-
-def create_folder(path)
-  Dir.mkdir(path) unless File.exists?(path)
-  FileUtils.chown(ENV['SSH_USER'], ENV['SSH_USER'], path)
+# Add Vagrant user to www-data group
+group node['nginx']['group'] do
+  action :modify
+  members 'vagrant'
+  append true
 end
 
 # Create directory /var/www.
-create_folder('/var/www')
+directory '/var/www' do
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  mode '0775'
+  action :create
+end
 
-if node['nginx-wrapper']['delete_default_dir'] && File.exist?('/var/www/html')
-  FileUtils.rm_r('/var/www/html')
+# Delete /var/www/html @TODO who create?
+if node['nginx-wrapper']['delete_default_dir']
+  directory '/var/www/html' do
+    action :delete
+  end
 end
 
 # Delete old config files.
-Dir.glob('/etc/nginx/sites-enabled/*.conf').each { |file| File.delete(file) }
-Dir.glob('/etc/nginx/sites-available/*.conf').each { |file| File.delete(file) }
+Dir.glob('/etc/nginx/sites-enabled/*.conf').each do |file|
+  file file do
+    action :delete
+  end
+end
+Dir.glob('/etc/nginx/sites-available/*.conf').each do |file|
+  file file do
+    action :delete
+  end
+end
 
 # nginx.site.conf templates
 if node.has_key?("nginx-wrapper") && node["nginx-wrapper"].has_key?("sites")
   node["nginx-wrapper"]["sites"].each do |key, site|
 
-    create_folder("/var/www/#{site['server_name']}")
+    directory "/var/www/#{site['server_name']}" do
+      owner node['nginx']['user']
+      group node['nginx']['group']
+      mode '0775'
+      action :create
+    end
 
     template "/etc/nginx/sites-available/#{site['server_name']}.conf" do
       source "nginx.site.conf.erb"
@@ -44,5 +65,6 @@ if node.has_key?("nginx-wrapper") && node["nginx-wrapper"].has_key?("sites")
     nginx_site "#{site['server_name']}.conf" do
       :enable
     end
+
   end
 end
