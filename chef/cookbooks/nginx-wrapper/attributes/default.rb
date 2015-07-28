dab5f710ed6c12081db426082384404cd742813a
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-28 22:46:03 +0600

    delete site.local from defaults

--- chef/cookbooks/nginx-wrapper/attributes/default.rb
+++ chef/cookbooks/nginx-wrapper/attributes/default.rb
@@ -1,7 +1,2 @@
 default['nginx-wrapper']['delete_default_dir'] = TRUE
-default['nginx-wrapper']['sites'] = {
-    'site' => {
-        'server_name' => 'site.local',
-        'port' => 80,
-    }
-}
+default['nginx-wrapper']['sites'] = {}
