ceb1d8a0463fde8f0a0bc63ebd41ed9cb12a3a0c
Sergey Sergin <sergey-sergin@kruhak.name>
2015-07-28 22:45:13 +0600

    fix php-fpm config

--- chef/cookbooks/php-fpm-wrapper/recipes/default.rb
+++ chef/cookbooks/php-fpm-wrapper/recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe "php-fpm"
 
 # Create php.ini file for php-fpm bases on php cookbook
 template "php.ini" do
-  path "#{node['php-fpm']['conf_dir']}/php.ini"
+  path "/etc/php5/fpm/php.ini"
   source "php.ini.erb"
   cookbook "php"
   variables(:directives => node['php']['directives'])
