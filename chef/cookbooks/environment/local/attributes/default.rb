7f2ba585705a94afb0cd88893a92f941ee80dae7
Sergey Sergin <sergey-sergin@kruhak.name>
2016-02-11 22:26:46 +0600

    Added nginx virtual host commentary

--- chef/cookbooks/environment/local/attributes/default.rb
+++ chef/cookbooks/environment/local/attributes/default.rb
@@ -3,4 +3,25 @@ default['vsftpd']['chroot'] = ['vagrant']
 default['vsftpd']['config']['listen_address'] = '0.0.0.0'
 
 default['nginx-wrapper']['user'] = 'vagrant'
+
+# Default configuration.
 default['nginx-wrapper']['sites']['site']['server_name'] = 'site.local'
+
+=begin
+
+Virtual host variables:
+
+server_name - site domain main (by default will be created folder /var/www/<server_name>)
+root - path to public files (Drupal core, as example) Default: /var/www/<server_name>/docroot
+port - virtual host port Default: 80
+index - index file (use carefully) Default: index.php
+
+Example virtual host configuration:
+
+Site with domain "drupal.local" located in directory "/home/drupal" listening port 8080
+
+default['nginx-wrapper']['sites']['site']['server_name'] = 'drupal.local'
+default['nginx-wrapper']['sites']['site']['root'] = '/home/drupal'
+default['nginx-wrapper']['sites']['site']['port'] = 8080
+
+=end
